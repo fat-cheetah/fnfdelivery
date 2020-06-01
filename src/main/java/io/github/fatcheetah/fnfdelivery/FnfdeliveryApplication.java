4c18e9bde341b0156ebe2064d29729035f2b86dc
fat-cheetah <ramanguleriamail@gmail.com>
2020-06-01 16:42:38 +0530

    Initialize Repo

---
+++ src/main/java/io/github/fatcheetah/fnfdelivery/FnfdeliveryApplication.java
@@ -0,0 +1,13 @@
+package io.github.fatcheetah.fnfdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnfdeliveryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FnfdeliveryApplication.class, args);
+  }
+
+}
